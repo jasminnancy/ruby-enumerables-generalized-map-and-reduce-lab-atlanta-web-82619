706f0ece62fd625fd4c46dea3b11cd3dd3d6361d
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-18 03:16:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,13 @@ end
 make_sandwich("chicken", "a sense of malaise") do |innards|
   puts "making some tasty stuff..."
 end
+
+
+def make_sandwich(element1, element2)
+  base = "A #{element1} and #{element2}"
+  yield(base)
+end
+ 
+make_sandwich("gator", "gumbo") do |innards|
+  "#{innards} on rye"
+end
