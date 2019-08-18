05ff134846cbe4102471cd7d58c66e5ec58cd527
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-18 03:31:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,9 +3,8 @@ def map(array)
   new_array = []
   
   while i < source_array.length do
-    x = source_array[i] * -1
     new_array << yield(array[i])
     i += 1
   end
-  return new_array
+  new_array
 end
