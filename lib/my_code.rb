8552fd98fb0b28c2ac1f0fb0b6beaaa612665356
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-18 03:26:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,6 +2,10 @@ def map(array)
   i = 0
   new_array = []
   
-  
-  
+  while i < source_array.length do
+    x = source_array[i] * -1
+    new_array.push(x)
+    i += 1
+  end
+  return new_array
 end
